90eb890ab26d2fdbc33926dab0b7a290b8b65ad8
thealidev <thealidevmail@gmail.com>
2026-05-13 16:27:02 +0500

    Integrated Google Tag Manager (GTM-5KPVVCZD) for Google Ads and Analytics tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,17 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5KPVVCZD');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/logo_final.png" />
         <script
           dangerouslySetInnerHTML={{
@@ -57,6 +68,16 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KPVVCZD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Providers>
           <div className="app-container">
             {children}
